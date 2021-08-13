8658691dedb57a080d11497869412eeb6d76208a
Sarah McClure <sarahmcclure97@gmail.com>
2021-08-12 18:49:22 -0700

    changing the test traffic:

--- test-traffic.py
+++ test-traffic.py
@@ -2,7 +2,7 @@ import sys
 from scapy.all import Ether, IP, UDP, VXLAN, sendp
 import time
 
-packet = Ether()/IP(dst="127.0.0.1")/UDP(dport=4789)/VXLAN(vni=2)
+packet = Ether()/IP(dst="10.4.0.6")/UDP(dport=4789)/VXLAN(vni=2)
 
 
 while True:
