b51f3eaa9ed2dd6e561db4b173946182a792f2a0
Sarah McClure <sarahmcclure97@gmail.com>
2021-09-30 16:11:07 -0700

    update test traffic for tos

--- test-traffic.py
+++ test-traffic.py
@@ -2,7 +2,7 @@ import sys
 from scapy.all import Ether, IP, UDP, VXLAN, sendp
 import time
 
-packet = Ether()/IP(src="127.0.0.1", dst="10.4.0.6")/UDP(dport=4789)/VXLAN(vni=2)
+packet = Ether()/IP(src="127.0.0.1", dst="10.4.0.6", tos=2)/UDP()
 
 
 while True:
