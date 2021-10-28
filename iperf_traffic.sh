a79f95caf858fcb9bf5f9c5cdd4c0a9d5f180267
Sarah McClure <sarahmcclure97@gmail.com>
2021-10-28 15:37:18 -0700

    script updates

--- iperf_traffic.sh
+++ iperf_traffic.sh
@@ -12,16 +12,19 @@ rm prios/prio3.1.out
 rm prios/prio3.2.out
 rm prios/prio4.out
 
+echo "Starting iperf traffic..."
 # First few high priority flows
 iperf3 -c 10.4.0.5  -t 100 -S 0x04 -b 10K --logfile prios/prio1.out -p 5021 &
 iperf3 -c 10.4.0.5  -t 100 -S 0x05 -b 110K --logfile prios/prio2.out -p 5022 &
 
 # Reach high priority limit (128K) [one of these should be admitted]
 iperf3 -c 10.4.0.5  -t 100 -S 0x06 -b 3K --logfile prios/prio3.1.out -p 5023
-sleep(2)
+sleep 2
 iperf3 -c 10.4.0.5  -t 100 -S 0x06 -b 3K --logfile prios/prio3.2.out -p 5024 &
 
 # Should be low priority
 iperf3 -c 10.4.0.5  -t 100 -S 0x07 -b 200K --logfile prios/prio4.out -p 5025
+echo "Test Complete"
 
+echo "Killing remote server processes"
 ssh -i ../receiver_key.pem azureuser@20.112.15.215 pkill iperf
