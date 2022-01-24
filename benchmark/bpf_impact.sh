07163210a7df480e95267832332dc4a32a7e04a6
Sarah McClure <sarahmcclure97@gmail.com>
2022-01-24 00:06:15 -0800

    fixing testing script

--- benchmark/bpf_impact.sh
+++ benchmark/bpf_impact.sh
@@ -9,7 +9,7 @@ FILE_NAME="default_tput_"
 
 #ssh -i $KEYPATH $REMOTE_USER@$REMOTE_IP iperf3 -s -p 5021 -D
 
-for i in {1..$TRIALS}
+for (( i=1; i<=$TRIALS; i++ ))
 do
    iperf3 -c $REMOTE_IP -t 60 -S 0x04 -p 5021 --logfile perf_tests/$FILE_NAME$i.out
 done
