29ca68afed870b35a31cb325b7fbe22758c1b2b2
Ubuntu <ubuntu@ip-10-10-1-36.us-east-2.compute.internal>
2022-01-28 07:07:53 +0000

    limit to 60 second trials for cpu

--- benchmark/top_total_cpu.sh
+++ benchmark/top_total_cpu.sh
@@ -2,7 +2,7 @@
 LOG_FILE="$1"
 PIDS="$2"
 
-top -b -d 2 -p $PIDS | awk \
+top -b -d 2 -n 30 -p $PIDS | awk \
     -v cpuLog="$LOG_FILE" -v pids="$PIDS" '
     /^top -/{FS=","; time = $1; time = substr(time, 7, 8); next}
     /Cpu/{FS=","; cpu = $4; split(cpu, a, " "); cpu = a[1];
