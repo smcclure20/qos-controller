33b85412bbb2f100143e4d7be101a0e7e46564a7
Ubuntu <ubuntu@ip-10-10-1-12.us-east-2.compute.internal>
2022-01-29 23:23:33 +0000

    update benching script

--- benchmark/start-hosts.sh
+++ benchmark/start-hosts.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-PER_NODE_HOSTS="1 400"
+PER_NODE_HOSTS="750 900"
 INTERVAL=10
 SERVER=10.10.1.36
 #SERVER_PIDS=""
 SERVER_THREADS=8
+BENCH_DIR="./cpu_bench_con6/"
 
 echo "Starting server with interval $INTERVAL"
 ssh -i ~/bpf-tester-key-aws.pem ubuntu@$SERVER "cd qos-controller && python3 tenant.py $SERVER -i $INTERVAL -t $SERVER_THREADS &>./log.out" &
@@ -24,13 +25,18 @@ do
         ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.123 "cd qos-controller && python3 host.py $SERVER:5000 10.10.1.123 -s $hpn -i $INTERVAL &>/dev/null" &
         ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.236 "cd qos-controller && python3 host.py $SERVER:5000 10.10.1.236 -s $hpn -i $INTERVAL &>/dev/null" &
         ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.107 "cd qos-controller && python3 host.py $SERVER:5000 10.10.1.107 -s $hpn -i $INTERVAL &>/dev/null" &
+	ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.100 "cd qos-controller && python3 host.py $SERVER:5000 10.10.1.100 -s $hpn -i $INTERVAL &>/dev/null" &
+	ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.57 "cd qos-controller && python3 host.py $SERVER:5000 10.10.1.57 -s $hpn -i $INTERVAL &>/dev/null" &
+	ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.90 "cd qos-controller && python3 host.py $SERVER:5000 10.10.1.90 -s $hpn -i $INTERVAL &>/dev/null" &
+	ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.38 "cd qos-controller && python3 host.py $SERVER:5000 10.10.1.38 -s $hpn -i $INTERVAL &>/dev/null" &
+	ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.83 "cd qos-controller && python3 host.py $SERVER:5000 10.10.1.83 -s $hpn -i $INTERVAL &>/dev/null" &
 
         python3 ./host.py $SERVER:5000 10.10.1.12 -s $hpn -i $INTERVAL &> log.out &
         
         sleep $INTERVAL
         echo "Beginning data collection..."
 
-        ssh -i ~/bpf-tester-key-aws.pem ubuntu@$SERVER "cd qos-controller/benchmark && ./top_total_cpu.sh ./cpu_bench_con4/cpu_5n_"$hpn"hpn_"$INTERVAL"i_"$SERVER_THREADS"t.txt $pids"
+        ssh -i ~/bpf-tester-key-aws.pem ubuntu@$SERVER "cd qos-controller/benchmark && ./top_total_cpu.sh "$BENCH_DIR"cpu_10n_"$hpn"hpn_"$INTERVAL"i_"$SERVER_THREADS"t.txt $pids"
         
         echo "done."
 
@@ -39,8 +45,14 @@ do
         ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.123 pkill python
         ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.236 pkill python
         ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.107 pkill python
+	ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.100 pkill python
+	ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.57 pkill python
+	ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.90 pkill python
+	ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.38 pkill python
+	ssh -i ~/bpf-tester-key-aws.pem ubuntu@10.10.1.83 pkill python
 
         sleep $INTERVAL
+	sleep $INTERVAL
 done
 fi
 
